cf8c1230bba2bde28a526811780a597d88a97391
slap-hand <130219709+slap-hand@users.noreply.github.com>
2023-04-08 21:40:15 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace colorized
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.red = new System.Windows.Forms.Button();
+            this.standard = new System.Windows.Forms.Button();
+            this.green = new System.Windows.Forms.Button();
+            this.orange = new System.Windows.Forms.Button();
+            this.purple = new System.Windows.Forms.Button();
+            this.white = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Sans Serif Collection", 25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(447, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 82);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Colorized";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.red.Font = new System.Drawing.Font("Sans Serif Collection", 10F, System.Drawing.FontStyle.Bold);
+            this.red.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.red.Location = new System.Drawing.Point(158, 50);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(100, 100);
+            this.red.TabIndex = 1;
+            this.red.Text = "red";
+            this.red.UseVisualStyleBackColor = false;
+            this.red.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // standard
+            // 
+            this.standard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.standard.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Bold);
+            this.standard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+            this.standard.Location = new System.Drawing.Point(43, 50);
+            this.standard.Name = "standard";
+            this.standard.Size = new System.Drawing.Size(100, 100);
+            this.standard.TabIndex = 2;
+            this.standard.Text = "standard";
+            this.standard.UseVisualStyleBackColor = false;
+            this.standard.Click += new System.EventHandler(this.standard_Click);
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.green.Font = new System.Drawing.Font("Sans Serif Collection", 10F);
+            this.green.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.green.Location = new System.Drawing.Point(43, 169);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(100, 100);
+            this.green.TabIndex = 3;
+            this.green.Text = "green";
+            this.green.UseVisualStyleBackColor = false;
+            this.green.Click += new System.EventHandler(this.green_Click);
+            // 
+            // orange
+            // 
+            this.orange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.orange.Font = new System.Drawing.Font("Sans Serif Collection", 9.5F, System.Drawing.FontStyle.Bold);
+            this.orange.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.orange.Location = new System.Drawing.Point(158, 169);
+            this.orange.Name = "orange";
+            this.orange.Size = new System.Drawing.Size(100, 100);
+            this.orange.TabIndex = 4;
+            this.orange.Text = "orange";
+            this.orange.UseVisualStyleBackColor = false;
+            this.orange.Click += new System.EventHandler(this.orange_Click);
+            // 
+            // purple
+            // 
+            this.purple.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.purple.Font = new System.Drawing.Font("Sans Serif Collection", 10F, System.Drawing.FontStyle.Bold);
+            this.purple.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.purple.Location = new System.Drawing.Point(43, 290);
+            this.purple.Name = "purple";
+            this.purple.Size = new System.Drawing.Size(100, 100);
+            this.purple.TabIndex = 5;
+            this.purple.Text = "purple";
+            this.purple.UseVisualStyleBackColor = false;
+            this.purple.Click += new System.EventHandler(this.purple_Click);
+            // 
+            // white
+            // 
+            this.white.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.white.Font = new System.Drawing.Font("Sans Serif Collection", 10F, System.Drawing.FontStyle.Bold);
+            this.white.ForeColor = System.Drawing.Color.White;
+            this.white.Location = new System.Drawing.Point(158, 290);
+            this.white.Name = "white";
+            this.white.Size = new System.Drawing.Size(100, 100);
+            this.white.TabIndex = 6;
+            this.white.Text = "white";
+            this.white.UseVisualStyleBackColor = false;
+            this.white.Click += new System.EventHandler(this.white_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(757, 450);
+            this.Controls.Add(this.white);
+            this.Controls.Add(this.purple);
+            this.Controls.Add(this.orange);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.standard);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button red;
+        private System.Windows.Forms.Button standard;
+        private System.Windows.Forms.Button green;
+        private System.Windows.Forms.Button orange;
+        private System.Windows.Forms.Button purple;
+        private System.Windows.Forms.Button white;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,387 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorized
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string red1 = "255 0 0";
+        string green1 = "0 255 0";
+        string orange1 = "255 167 43";
+        string purple1 = "173 110 255";
+        string white1 = "255 255 255";
+        string default1 = "153 180 209";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "" + red1 + "";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + red1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + red1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + red1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+        //red
+
+        private void standard_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "153 180 209";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + default1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + default1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + default1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+        //standard
+
+        private void green_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "" + green1 + "";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + green1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + green1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + green1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+        //green
+
+        private void orange_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "" + orange1 + "";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + orange1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + orange1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + orange1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+        //orange
+
+        private void purple_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "" + purple1 + "";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + purple1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + purple1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + purple1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+        //purple
+
+        private void white_Click(object sender, EventArgs e)
+        {
+            // Specify the path to the registry key and the value you want to change
+            string keyPath = @"Control Panel\Colors";
+            string valueName = "Window";
+            string valueData = "" + white1 + "";
+
+            // Open the registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Set the value of the specified key
+            key.SetValue(valueName, valueData);
+
+            // Close the registry key
+            key.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath1 = @"Control Panel\Colors";
+            string valueName1 = "Hilight";
+            string valueData1 = "" + white1 + "";
+
+            // Open the registry key
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(keyPath1, true);
+
+            // Set the value of the specified key
+            key1.SetValue(valueName1, valueData1);
+
+            // Close the registry key
+            key1.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath2 = @"Control Panel\Colors";
+            string valueName2 = "HotTrackingColor";
+            string valueData2 = "" + white1 + "";
+
+            // Open the registry key
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(keyPath2, true);
+
+            // Set the value of the specified key
+            key2.SetValue(valueName2, valueData2);
+
+            // Close the registry key
+            key2.Close();
+
+            // Specify the path to the registry key and the value you want to change
+            string keyPath3 = @"Control Panel\Colors";
+            string valueName3 = "WindowFrame";
+            string valueData3 = "" + white1 + "";
+
+            // Open the registry key
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(keyPath3, true);
+
+            // Set the value of the specified key
+            key3.SetValue(valueName3, valueData3);
+
+            // Close the registry key
+            key3.Close();
+        }
+    }
+}
